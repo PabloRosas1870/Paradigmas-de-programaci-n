7e05e4ee157dcbeaaf42837494fa79d791bfa455
PabloRosas1870 <99694964+PabloRosas1870@users.noreply.github.com>
2022-06-17 16:43:26 -0500

    Add files via upload

---
+++ calcular_e.py
@@ -0,0 +1,23 @@
+#====================================
+#Algoritmo 1 
+#==================================
+
+#Serie exponencial 
+#factorización de  x 
+#Negativos de la funcion inversa 
+#==================================
+
+n=200
+x =-100.0
+flag=False
+if x<0:
+	flag= True
+	x=-x
+s = 1.0
+for i in range(n,0,-1):
+	s *=x/float(i)
+	s +=1.0
+if flag == True:
+	s=1/s
+
+print (s)
