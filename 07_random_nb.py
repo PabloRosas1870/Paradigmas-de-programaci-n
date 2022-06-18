3fd6412a33de601ab0428662935f2bc20c198f29
PabloRosas1870 <99694964+PabloRosas1870@users.noreply.github.com>
2022-06-17 21:00:35 -0500

    Add files via upload

---
+++ 07_random_nb.py
@@ -0,0 +1,19 @@
+#==========================
+#Forma compacta de random_nb
+#=========================
+import numpy 
+from mpi4py import MPI
+comm= MPI.COMM_WORLD
+rank= comm.Get_rank()
+
+randNum = numpy.zeros(1)
+if rank ==1:
+	dst =0 
+	src=0
+if rank == 0:
+	dst=1
+	src=1
+randNum = numpy.random.random_sample(1)
+print ("Process", rank"Drew the number", randNum[0])
+
+	
